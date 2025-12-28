a9ad6eaa814ee4578cbeb1e370b53a628f8c106a
antellics <122561209+antellics@users.noreply.github.com>
2025-12-28 00:16:42 -0500

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -14,6 +14,16 @@ def emotion_detector(text_to_analyse: str) -> dict:
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header, timeout=10)
+    if response.status_code == 400:
+        anger_score = disgust_score = fear_score = joy_score = sadness_score = dominant_emotion = None
+        return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+        }
     formatted_response = json.loads(response.text)
     if formatted_response.get("emotionPredictions"):
         emotions = formatted_response.get("emotionPredictions")[0].get("emotion")
