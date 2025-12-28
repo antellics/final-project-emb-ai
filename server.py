9d0e39b8bc9c4714760a523701ab2e6e65d2cc6f
antellics <122561209+antellics@users.noreply.github.com>
2025-12-27 23:59:18 -0500

    Create server.py

---
+++ server.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3.11
+"""
+emotionDetector server component
+"""
+
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def emot_detector():
+    """
+    Call to NLP tools
+    """
+    result = emotion_detector(request.args.get("textToAnalyze"))
+    if result.get('dominant_emotion'):
+        response_text = "For the given statement, the system response is "
+        response_text += f"'anger': {result.get('anger')}, "
+        response_text += f"'disgust': {result.get('disgust')}, "
+        response_text += f"'fear': {result.get('fear')}, "
+        response_text += f"'joy': {result.get('joy')} and "
+        response_text += f"'sadness': {result.get('sadness')}. "
+        response_text += f"The dominant emotion is <b>{result.get('dominant_emotion')}</b>."
+    
+        return response_text
+    return "Invalid input ! Try again."
+
+@app.route("/")
+def render_index_page():
+    """
+    Default route
+    """
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
