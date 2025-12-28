b7c44868db7400020c9870c325c49187641a6e94
antellics <122561209+antellics@users.noreply.github.com>
2025-12-28 00:21:37 -0500

    Update server.py

--- server.py
+++ server.py
@@ -16,16 +16,14 @@ def emot_detector():
     result = emotion_detector(request.args.get("textToAnalyze"))
     if result.get('dominant_emotion') is None:
         return "Invalid input ! Try again."
-    else:
-        response_text = "For the given statement, the system response is "
-        response_text += f"'anger': {result.get('anger')}, "
-        response_text += f"'disgust': {result.get('disgust')}, "
-        response_text += f"'fear': {result.get('fear')}, "
-        response_text += f"'joy': {result.get('joy')} and "
-        response_text += f"'sadness': {result.get('sadness')}. "
-        response_text += f"The dominant emotion is <b>{result.get('dominant_emotion')}</b>."
-    
-        return response_text
+    response_text = "For the given statement, the system response is "
+    response_text += f"'anger': {result.get('anger')}, "
+    response_text += f"'disgust': {result.get('disgust')}, "
+    response_text += f"'fear': {result.get('fear')}, "
+    response_text += f"'joy': {result.get('joy')} and "
+    response_text += f"'sadness': {result.get('sadness')}. "
+    response_text += f"The dominant emotion is <b>{result.get('dominant_emotion')}</b>."
+    return response_text
 
 
 @app.route("/")
