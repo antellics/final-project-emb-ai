9fea019701fa5bfc93c4338b27c0c89f8da34b35
antellics <122561209+antellics@users.noreply.github.com>
2025-12-27 22:29:14 -0500

    Create emotion_detection.py

---
+++ emotion_detection.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3.11
+"""
+Emotion detection
+"""
+import requests
+
+def emotion_detector(text_to_analyse: str) -> dict:
+    """
+    Interface to IBM NLP libraries
+    """
+    url = "https://sn-watson-emotion.labs.skills.network/v1/"\
+    "watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    response = requests.post(url, json = myobj, headers=header, timeout=10)
+    return response.text
